d604dd6a010f880da8565a0149f02bcb347b4d37
gabriel-rosa-da-silva-nunes <gabrielrosadasilvanunes@gmail.com>
2025-04-05 13:59:45 -0300

    feat: add kubernetes configuration

---
+++ kubernetes/kubernetes_up.sh
@@ -0,0 +1,8 @@
+docker build -t video-worker-app:latest .
+
+kubectl create secret generic aws-secrets \
+  --from-literal=access-key-id=$(aws configure get aws_access_key_id) \
+  --from-literal=secret-access-key=$(aws configure get aws_secret_access_key) \
+  --from-literal=access-session-token=$(aws configure get aws_session_token)
+kubectl apply -f kubernetes/video-worker-deployment.yaml
+kubectl apply -f kubernetes/video-worker-hpa.yaml
