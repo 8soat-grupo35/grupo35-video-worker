e946523de5ba5e0e5e5e8a925448ef105d2b380a
gabriel-rosa-da-silva-nunes <gabrielrosadasilvanunes@gmail.com>
2025-04-05 13:08:23 -0300

    feat: docker configuration

--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"grupo35-video-worker/internal/handlers"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 )
 
 func main() {
+	fmt.Println("Starting video worker")
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 
 	if err != nil {
+		fmt.Println(err)
 		panic(err)
 	}
 
