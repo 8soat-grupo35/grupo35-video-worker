e503cc6d6fd145c2f41b5633d64c1c1fe94869c6
gabriel-rosa-da-silva-nunes <gabrielrosadasilvanunes@gmail.com>
2025-04-22 22:01:50 -0300

    fix: change secrets name to unique one

--- kubernetes/kubernetes_up.sh
+++ kubernetes/kubernetes_up.sh
@@ -1,6 +1,6 @@
 docker build -t video-worker-app:latest .
 
-kubectl create secret generic aws-secrets \
+kubectl create secret generic aws-video-worker-secrets \
   --from-literal=access-key-id=$(aws configure get aws_access_key_id) \
   --from-literal=secret-access-key=$(aws configure get aws_secret_access_key) \
   --from-literal=access-session-token=$(aws configure get aws_session_token)
